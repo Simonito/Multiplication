1528a64d37833fd312a91db61879e339856ab6ca
Šimon Ukuš <xkus@stuba.sk>
2022-06-05 22:38:20 +0200

    karatsuba alg implementation for numbers as strings

--- src/main/java/multiplication/Multiplication.java
+++ src/main/java/multiplication/Multiplication.java
@@ -3,4 +3,10 @@ package multiplication;
 public class Multiplication {
 	
 	
+	public static String karatsuba(String num1, String num2) {
+		// base case
+		if(num1.length() == 1 || num2.length() == 1) {
+			return Integer.toString(Integer.parseInt(num1) * Integer.parseInt(num2));
+		}
+	}
 }
